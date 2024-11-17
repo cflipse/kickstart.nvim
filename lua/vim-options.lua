e9d55fdbf4650fd19817a2f6625d5d3a34e83286
Chris Flipse <cflipse@gmail.com>
2024-11-17 10:27:21 -0500

    add folding config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -60,3 +60,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Fold by syntax
+vim.opt.foldmethod = 'syntax'
+vim.opt.foldlevel = 10
