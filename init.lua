8a533ea62470014da419f03b5f4294134113848c
Chris Flipse <cflipse@gmail.com>
2025-03-28 12:34:35 -0400

    add separate diagnostic config

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require 'vim-options'
 require 'keymaps'
+require 'diagnostic'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
